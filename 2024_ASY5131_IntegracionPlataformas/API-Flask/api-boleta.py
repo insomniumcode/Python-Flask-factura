21009b51c04168dceef6494db14e2666200f2ff7
insomniumcode <111256576+insomniumcode@users.noreply.github.com>
2024-06-06 15:00:17 -0400

    fecha y hora para que se muestre en factura

--- 2024_ASY5131_IntegracionPlataformas/API-Flask/api-boleta.py
+++ 2024_ASY5131_IntegracionPlataformas/API-Flask/api-boleta.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_restful import Api, Resource
 import requests
 from flask_cors import CORS
+from datetime import datetime
 
 
 
@@ -15,21 +16,22 @@ app = Flask(__name__)
 
 factura = {
     'cliente': '',
-    'items': []
+    'items': [],
+    'fecha_hora': ''
 }
 
 
-# Función para calcular el total de la factura
+# calcular el total 
 def calcular_total(items):
     return sum(item['precio'] * item['cantidad'] for item in items)
 
-# Ruta para mostrar la factura en formato HTML
+#  factura en  HTML
 @app.route('/factura', methods=['GET'])
 def mostrar_factura():
     total = calcular_total(factura['items'])
     return render_template('factura.html', factura=factura, total=total)
 
-# Ruta para agregar un item a la factura
+#agregar un item a la factura
 @app.route('/agregar_item', methods=['GET', 'POST'])
 def agregar_item():
     if request.method == 'POST':
@@ -37,25 +39,29 @@ def agregar_item():
         producto_nombreproducto = request.form['producto']
         cantidad = int(request.form['cantidad'])
         
-        # Llamar a la API de C# para obtener los datos del cliente
+        # obtener los datos del cliente
         cliente_response = requests.get(f'http://localhost:7777/api/cliente/{cliente_razonSocial}')
         cliente_data = cliente_response.json()
         factura['cliente'] = cliente_data['razonSocial']
 
+        #  para obtener los datos del producto
         cliente_response = requests.get(f'http://localhost:5000/api/producto/{producto_nombreproducto}')
         cliente_data = cliente_response.json()
         factura['Producto'] = cliente_data['nombreproducto']
         
-        # Llamar a la API de C# para obtener los datos del producto
+        #  para obtener los datos del producto
         producto_response = requests.get(f'http://localhost:5000/api/producto/{producto_nombreproducto}')
         producto_data = producto_response.json()
         producto_data['cantidad'] = cantidad
         
         factura['items'].append(producto_data)
+
+        factura['fecha_hora'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         
         return redirect(url_for('mostrar_factura'))
     
-    # Llamar a las APIs para obtener la lista de clientes y productos
+    # obtener lista de clientes y productos
     clientes_response = requests.get('http://localhost:7777/api/cliente/')
     productos_response = requests.get('http://localhost:5000/api/producto/')
     clientes = clientes_response.json()
