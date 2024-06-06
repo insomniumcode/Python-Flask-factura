5f169f38f291f6589b19625270360d2236b0f989
insomniumcode <111256576+insomniumcode@users.noreply.github.com>
2024-06-06 16:03:55 -0400

    se agrega css a factura y a agregar item

--- 2024_ASY5131_IntegracionPlataformas/API-Csharp/InversionesLtda.API/obj/Debug/netcoreapp3.1/InversionesLtda.API.AssemblyInfo.cs
+++ 2024_ASY5131_IntegracionPlataformas/API-Csharp/InversionesLtda.API/obj/Debug/netcoreapp3.1/InversionesLtda.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InversionesLtda.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3580db347d1bf2d98463623e3b711551455ad779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab923a5c10ea6a6ffa297a35acd4f9ae23ced9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InversionesLtda.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InversionesLtda.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
